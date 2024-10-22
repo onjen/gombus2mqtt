6b469c4f4af50eaa964bf9ed169f53210f4963a6
Johannes Rothe <mail@johannes-rothe.de>
2024-10-22 22:08:08 +0200

    Use slog package for logging

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"log/slog"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+var debugFlag = flag.Bool("d", false, "Set loglevel to debug")
+
 func main() {
+	flag.Parse()
+	if *debugFlag {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
 	config, err := parseConfig("config.yaml")
 	if err != nil {
 		log.Fatalf("Error parsing config: %v", err)
@@ -34,7 +43,24 @@ func main() {
 	}
 }
 
+func createMQTTClient(config Config) (mqtt.Client, error) {
+	o := mqtt.NewClientOptions()
+
+	for _, s := range config.Servers {
+		o.AddBroker(s)
+	}
+	o.SetClientID(config.ClientID)
+
+	o.SetOnConnectHandler(func(client mqtt.Client) {
+		slog.Info("Connected to the broker")
+	})
+
+	client := mqtt.NewClient(o)
+	return client, nil
+}
+
 func fetchAndPublish(client mqtt.Client, topic string) {
 	val := fetchValue()
 	client.Publish(topic, 0, false, fmt.Sprint(val))
+	slog.Debug("Fetched new value and published to MQTT", "value", val)
 }
